40ee89c87488d67bbd97a0027fed029e4304961c
hitode909 <hitode909@gmail.com>
2020-07-23 20:25:53 +0900

    indent

--- lib/App/UpdateCPANfile/CPANfileSnapshotParser.pm
+++ lib/App/UpdateCPANfile/CPANfileSnapshotParser.pm
@@ -5,18 +5,18 @@ use warnings;
 use CPAN::DistnameInfo;
 
 sub scan_deps {
-  my ($class, $path) = @_;
+    my ($class, $path) = @_;
 
-  open my $fh, '<', $path or die $!;
+    open my $fh, '<', $path or die $!;
 
-  my @deps;
-  while ( defined( my $line = <$fh> ) ) {
-     if ( $line =~ m/pathname: ([^\s]+)/ ) {
-       push @deps, CPAN::DistnameInfo->new($1);
-     }
-  }
+    my @deps;
+    while ( defined( my $line = <$fh> ) ) {
+       if ( $line =~ m/pathname: ([^\s]+)/ ) {
+         push @deps, CPAN::DistnameInfo->new($1);
+       }
+    }
 
-  \@deps;
+    \@deps;
 }
 
 1;
--- lib/App/UpdateCPANfile/PackageDetails.pm
+++ lib/App/UpdateCPANfile/PackageDetails.pm
@@ -7,35 +7,35 @@ use Furl;
 use IO::String;
 
 sub new {
-  my ($class) = @_;
+    my ($class) = @_;
 
-  bless {}, $class;
+    bless {}, $class;
 }
 
 sub details {
-  my ($self) = @_;
-  if ($self->{details}) {
-    return $self->{details};
-  }
+    my ($self) = @_;
+    if ($self->{details}) {
+        return $self->{details};
+    }
 
-  my $furl = Furl->new(
-    agent   => 'UpdateCPANfile/0.1',
-    timeout => 60,
-  );
+    my $furl = Furl->new(
+        agent   => 'UpdateCPANfile/0.1',
+        timeout => 60,
+    );
 
-  my $file = IO::String->new;
-  my $res = $furl->get('http://www.cpan.org/modules/02packages.details.txt.gz');
-  $file->print($res->content);
-  $file->setpos(0);
+    my $file = IO::String->new;
+    my $res = $furl->get('http://www.cpan.org/modules/02packages.details.txt.gz');
+    $file->print($res->content);
+    $file->setpos(0);
 
-  $self->{details} = CPAN::PackageDetails->read( $file );
+    $self->{details} = CPAN::PackageDetails->read( $file );
 }
 
 sub latest_version_for_package {
-  my ($self, $package) = @_;
-  my ($package_object) = $self->details->entries->get_entries_by_package($package);
-  return undef unless $package_object;
-  return $package_object->version;
+    my ($self, $package) = @_;
+    my ($package_object) = $self->details->entries->get_entries_by_package($package);
+    return undef unless $package_object;
+    return $package_object->version;
 }
 
 1;
