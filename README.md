6241009f8f971fda265e1e6b862ac056c82c3797
hitode909 <hitode909@gmail.com>
2020-07-23 12:53:26 +0900

    minil new App::UpdateCPANfile

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::UpdateCPANfile - It's new $module
+
+# SYNOPSIS
+
+    use App::UpdateCPANfile;
+
+# DESCRIPTION
+
+App::UpdateCPANfile is ...
+
+# LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hitode909 <hitode909@gmail.com>
---
+++ lib/App/UpdateCPANfile.pm
@@ -0,0 +1,39 @@
+package App::UpdateCPANfile;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::UpdateCPANfile - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::UpdateCPANfile;
+
+=head1 DESCRIPTION
+
+App::UpdateCPANfile is ...
+
+=head1 LICENSE
+
+Copyright (C) hitode909.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hitode909 E<lt>hitode909@gmail.comE<gt>
+
+=cut
+
