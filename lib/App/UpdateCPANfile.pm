6ce65e589cd9aef1ab9761b1b47afcd80ad60c28
hitode909 <hitode909@gmail.com>
2020-07-28 14:16:02 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 5a0b206..625914f 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension App-UpdateCPANfile
     
     {{$NEXT}}
     
    +0.05 2020-07-28T05:15:39Z
    +    - Make core modules detection better.
    +
     0.04 2020-07-28T01:35:26Z
         - Changed to specify exact version.

--- lib/App/UpdateCPANfile.pm
+++ lib/App/UpdateCPANfile.pm
@@ -9,7 +9,7 @@ use App::UpdateCPANfile::PackageDetails;
 use CPAN::DistnameInfo;
 use Module::CoreList;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 sub new {
     my ($class, $path, $snapshot_path, $options) = @_;
